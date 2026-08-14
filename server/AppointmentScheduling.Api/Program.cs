5f32187484cf07398d17d06676df74a2375b482d
Jian Chiang Poh <jian.poh@ou.ac.uk>
2026-08-14 01:33:26 +0100

    chore: standardise language and remove templates

--- server/AppointmentScheduling.Api/Program.cs
+++ server/AppointmentScheduling.Api/Program.cs
@@ -4,8 +4,6 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<AppointmentDbContext>(options =>
@@ -24,7 +22,6 @@ builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
